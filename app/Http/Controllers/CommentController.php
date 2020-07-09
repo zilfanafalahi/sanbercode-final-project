b94835af461e10009c2974a6b252dac593228554
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-07-10 00:48:29 +0800

    Controller comment for store and show

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,49 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
+use App\Answer;
+use App\Comment;
 
 class CommentController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+{   
+    // Question - comment
+    public function show_question($id)
     {
-        //
+        $question = Question::find($id);
+        $comments = $question -> comments;
+        
+        return view('comments.index_question', compact('question','comments'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+
+    public function store_question(Request $request, $id)
     {
-        //
+        $question = Question::find($id);
+        unset($request["_token"]);
+        // dd($request->all());
+        $new_comment = Comment::create($request->all());
+        $question -> comments() -> attach($new_comment);
+        return redirect('/questions/'.$id.'/comments');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+
+    // Answer-comments
+    public function show_answer($q_id,$id)
+    {   
+        $question = Question::find($q_id);
+        $answer = Answer::find($id);
+        $comments = $answer -> comments;
+        
+        return view('comments.index_answer', compact('question','answer','comments'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+
+    public function store_answer(Request $request, $q_id, $id)
     {
-        //
+        $answer = answer::find($id);
+        unset($request["_token"]);
+        $new_comment = Comment::create($request->all());
+        $answer -> comments() -> attach($new_comment);
+        // dd($new_comment);
+        return redirect("/answers/$q_id/$id/comments");
     }
 
     /**
