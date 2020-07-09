8c6e7dec6f8282fbbad146c75d663e20f87827cb
zilfanafalahi <zilfanafalahi45@gmail.com>
2020-07-09 10:22:50 +0700

    Menambahkan Library Editor

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
