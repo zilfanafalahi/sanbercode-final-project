f62b16681dcf54efbb3d04748042e8bb3206ff55
tasyaadinda <tasyaadinda03@gmail.com>
2020-07-10 14:56:42 +0700

    create logo

--- resources/views/adminlte/partials/sidebar.blade.php
+++ resources/views/adminlte/partials/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="../../index3.html" class="brand-link">
-    <img src="{{asset('/adminlte/dist/img/AdminLTELogo.png')}}"
-        alt="AdminLTE Logo"
+    <img src="{{asset('/adminlte/dist/img/67Dev.jpg')}}"
+        alt="67 Logo"
         class="brand-image img-circle elevation-3"
         style="opacity: .8">
     <span class="brand-text font-weight-light">Kelompok 67</span>
