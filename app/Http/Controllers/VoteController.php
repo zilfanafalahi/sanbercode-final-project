aaeb56862172b997914246d42b68ff2761ea03ad
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-07-11 17:43:37 +0800

    add down and up votes for question

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -3,82 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Question;
+use App\Answer;
+use App\Vote;
+
 
 class VoteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function upvotes_question($q_id)
+    {   
+        $question = Question::find($q_id);
+        $new_vote = Vote::create([
+            'poin' => 1,
+            'voter_user_id' => Auth::id()
+        ]);
+        $question -> votes() -> attach($new_vote);
+        return redirect('/questions');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    public function downvotes_question($q_id)
+    {   
+        $question = Question::find($q_id);
+        $new_vote = Vote::create([
+            'poin' => -1,
+            'voter_user_id' => Auth::id()
+        ]);
+        $question -> votes() -> attach($new_vote);
+        return redirect('/questions');
     }
 }
